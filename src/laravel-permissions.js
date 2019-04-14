b0f6680c856f33508046e33ed96354474d53b33c
John Cruz <sv.johncruz@gmail.com>
2019-04-14 17:25:14 -0600

    ✨ set/get Permissions, set/get Roles added

--- src/laravel-permissions.js
+++ src/laravel-permissions.js
@@ -2,6 +2,18 @@ export default {
   install(Vue, _options) {
     window.Laravel = {};
 
-    //
+    Vue.prototype.$laravel = {
+      setPermissions: (permissions) => {
+        window.Laravel.permissions = permissions;
+      },
+
+      getPermissions: () => window.Laravel.permissions,
+
+      setRoles: (roles) => {
+        window.Laravel.roles = roles;
+      },
+
+      getRoles: () => window.Laravel.roles,
+    };
   }
 };
