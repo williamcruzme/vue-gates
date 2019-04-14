a9760e2103610ddfdea28a67126258f50b04281e
John Cruz <sv.johncruz@gmail.com>
2019-04-14 17:32:07 -0600

    ✨ Apply directive when condition is true

--- src/laravel-permissions.js
+++ src/laravel-permissions.js
@@ -1,3 +1,29 @@
+// Apply directive when condition is true
+const when = (condition) => (el, binding) => {
+  if (!binding.value) {
+    return console.error('You must specify a value in the directive.');
+  }
+
+  // Get property name based on directive name
+  const property = binding.name === 'can' ? 'permissions' : 'roles';
+
+  // Only allow this function to be run if the Laravel instance exists
+  if (!window.Laravel || !window.Laravel.hasOwnProperty(property)) {
+    return;
+  }
+
+  // Check if value exists in property value
+  if (!condition(window.Laravel[property], binding.value)) {
+    if (!binding.arg) {
+      // Remove DOM Element
+      el.parentNode.removeChild(el);
+    } else {
+      // Set 'true' custom property
+      el[binding.arg] = true;
+    }
+  }
+};
+
 export default {
   install(Vue, _options) {
     window.Laravel = {};
