35fd1fd82f7e7a1898ae992f5b877291cbf65980
williamcruzme <sv.johncruz@gmail.com>
2019-05-17 22:25:22 -0600

    💡 Missing comment in "can" alias directive added

--- src/laravel-permissions.js
+++ src/laravel-permissions.js
@@ -117,6 +117,7 @@ export default {
     Vue.directive('can', {
       inserted: (el, binding) => {
         if (!Vue.prototype.$laravel.hasPermission(binding.value)) {
+          // Remove DOM Element
           el.parentNode.removeChild(el);
         }
       },
