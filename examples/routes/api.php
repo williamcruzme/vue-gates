335c96fed5a2a489d7189367beba238a85d04982
John Cruz <jcruz@applaudostudios.com>
2020-08-03 20:47:08 -0600

    ♻ API middlewares refactor

--- examples/routes/api.php
+++ examples/routes/api.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::namespace('Auth')->group(function () {
+Route::namespace('Auth')->middleware('auth')->group(function () {
     Route::get('permissions', 'PermissionController');
     Route::get('roles', 'RoleController');
 });
