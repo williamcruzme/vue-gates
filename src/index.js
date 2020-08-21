cb463e9f2833f7ed6814cddb68dc979ac14fc5a5
John Cruz <jcruz@applaudostudios.com>
2020-08-21 11:16:13 -0600

    🏗 Project structure updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
-import NuxtModule from './module';
-import VuePlugin from './plugin';
+import Gate from './core/gate';
+import { isConditionPassed } from './utils/validator';
 
-export default process.browser ? VuePlugin : NuxtModule;
+const registerDirectives = (Vue) => {
+  const directiveOptions = {
+    inserted: isConditionPassed(Vue),
+  };
+
+  Vue.directive('permission', directiveOptions);
+  Vue.directive('can', directiveOptions); // Alias for "v-permission"
+  Vue.directive('role', directiveOptions);
+  Vue.directive('role-or-permission', {
+    inserted: (el, binding) => {
+      if (!binding.value) {
+        console.error('You must specify a value in the directive.');
+        return;
+      }
+
+      const values = binding.value.split('|');
+      const role = values[0];
+      const permission = values[1];
+
+      if (
+        !Vue.prototype.$gates.hasRole(role)
+        && !Vue.prototype.$gates.hasPermission(permission)
+      ) {
+      // Remove DOM Element
+        el.parentNode.removeChild(el);
+      }
+    },
+  });
+};
+
+export default {
+  install(Vue, options = {}) {
+    const gate = new Gate(options);
+
+    Vue.prototype.$gates = gate;
+    Vue.gates = gate;
+
+    registerDirectives(Vue);
+  },
+};
