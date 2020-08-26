f0caf8536e13b7b7d005d2fd04f532ca9b0ae121
John Cruz <jcruz@applaudostudios.com>
2020-08-26 17:19:10 -0600

    🎨 Formatting

--- examples/resources/js/components/ExampleComponent.vue
+++ examples/resources/js/components/ExampleComponent.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card" v-role="'user'">
-                    <div class="card-header">Example Component</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card" v-role="'user'">
+          <div class="card-header">Example Component</div>
 
-                    <div class="card-body">
-                        <button v-can:disabled="'click yeah'">Yeah!</button>
-                    </div>
-                </div>
-            </div>
+          <div class="card-body">
+            <button v-can:disabled="'click yeah'">Yeah!</button>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        async created() {
-            const { data: roles } = await axios.get('/api/roles');
-            const { data: permissions } = await axios.get('/api/permissions');
+export default {
+  async created() {
+    const { data: roles } = await axios.get('/api/roles');
+    const { data: permissions } = await axios.get('/api/permissions');
 
-            this.$gates.setRoles(roles);
-            this.$gates.setPermissions(permissions);
-        }
-    }
+    this.$gates.setRoles(roles);
+    this.$gates.setPermissions(permissions);
+  },
+};
 </script>
