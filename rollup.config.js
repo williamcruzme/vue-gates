1798771a7e4dd5d0a528e084a6e10a492ce066be
John Cruz <jcruz@applaudostudios.com>
2020-09-28 09:03:05 -0600

    🔧 Rollup Vue support added

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,34 @@
 import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import buble from '@rollup/plugin-buble';
+import replace from '@rollup/plugin-replace';
+import vue from 'rollup-plugin-vue';
 
-export default [
-  {
-    input: 'src/index.js',
-    output: {
-      name: 'VueGates',
-      env: 'production',
-      format: 'umd',
-      file: 'dist/vue-gates.js',
-    },
-    plugins: [
-      resolve(),
-      babel({
-        babelHelpers: 'bundled',
-        exclude: 'node_modules/**',
-      }),
-    ],
+export default {
+  input: 'src/index.js',
+  output: {
+    name: 'VueGates',
+    file: 'dist/vue-gates.js',
+    format: 'umd',
+    exports: 'named',
+    globals: { vue: 'Vue' },
   },
-];
+  external: ['vue'],
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
+    commonjs(),
+    vue({
+      css: false,
+      template: {
+        isProduction: true,
+      },
+    }),
+    buble(),
+  ],
+};
