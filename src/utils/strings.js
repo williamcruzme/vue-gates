615f4cb2f5a51f7944ebc4b19011ff9d9f072d9c
williamcruzme <sv.johncruz@gmail.com>
2021-04-21 00:27:48 -0600

    🎨 Formatting

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -2,11 +2,9 @@ export const startCase = (str) => `${str.charAt(0).toUpperCase()}${str.slice(1)}
 
 export const isEmpty = (obj) => Object.keys(obj).length === 0;
 
-export const pregQuote = (str) => {
-  return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
-};
+export const pregQuote = (str) => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
 
 export const match = (str, wildcard) => {
-  const regex = new RegExp('^' + wildcard.split(/\*+/).map(pregQuote).join('.*') + '$', 'g');
+  const regex = new RegExp(`^${wildcard.split(/\*+/).map(pregQuote).join('.*')}$`, 'g');
   return str.match(regex);
 };
