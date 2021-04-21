9a9dd215ed714fb5b3bba52e35a108cff5985c74
williamcruzme <sv.johncruz@gmail.com>
2021-04-20 23:50:28 -0600

    🐛 Match permission/role with wildcard improved

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -2,12 +2,11 @@ export const startCase = (str) => `${str.charAt(0).toUpperCase()}${str.slice(1)}
 
 export const isEmpty = (obj) => Object.keys(obj).length === 0;
 
-export const pregQuote = (str, delimiter) => {
-  const regex = new RegExp(`[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\${delimiter || ''}-]`, 'g');
-  return (`${str}`).replace(regex, '\\$&');
+export const pregQuote = (str) => {
+  return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
 };
 
 export const match = (str, wildcard) => {
-  const regex = new RegExp(pregQuote(wildcard).replace(/\\\*/g, '.*').replace(/\\\?/g, '.'), 'g');
+  const regex = new RegExp('^' + wildcard.split(/\*+/).map(pregQuote).join('.*') + '$', 'g');
   return str.match(regex);
 };
